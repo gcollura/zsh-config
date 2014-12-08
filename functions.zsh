b964bad7f5947f476a9f2c0daf77032d0a4c4479
Giulio Collura <random.cpp@gmail.com>
2014-12-08 20:24:05 +0100

    Add ssh and trw functions

--- functions.zsh
+++ functions.zsh
@@ -69,3 +69,38 @@ function extract () {
         echo "'$1' is not a valid file"
     fi
 }
+
+# ssh wrapper that rename current tmux window to the hostname of the
+# remote host.
+function ssh () {
+    # Do nothing if we are not inside tmux or ssh is called without arguments
+    if [[ $# == 0 || -z $TMUX ]]; then
+        command ssh $@
+        return
+    fi
+    # The hostname is the last parameter (i.e. ${(P)#})
+    local remote=${${(P)#}%.*}
+    local old_name="$(tmux display-message -p '#W')"
+    local renamed=0
+    # Save the current name
+    if [[ $remote != -* ]]; then
+        renamed=1
+        tmux rename-window $remote
+    fi
+    command ssh $@
+    if [[ $renamed == 1 ]]; then
+        tmux rename-window "$old_name"
+    fi
+}
+
+# Tmux
+function trw () {
+    if [[ -z $TMUX ]]; then
+        return
+    fi
+    local window=`basename $PWD`
+    if [[ -n $1 ]]; then
+        window="$1"
+    fi
+    tmux rename-window "$window"
+}
