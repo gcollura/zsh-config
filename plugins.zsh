b36ebfad06b334a41c272c20e62742025400736a
Giulio Collura <random.cpp@gmail.com>
2014-12-20 00:50:52 +0100

    Fix TMPDIR set to a certain value in some terminals, disable tmux autostart

--- plugins.zsh
+++ plugins.zsh
@@ -1,6 +1,8 @@
 # put plugin configurations here
-ZSH_TMUX_AUTOSTART=true
+ZSH_TMUX_AUTOSTART=false
 ZSH_TMUX_ITERM2=false
+# sometimes TMPDIR is set to a certain value, so disable this
+unset TMPDIR
 
 source $ZSH/plugins/git.zsh
 source $ZSH/plugins/history-substring-search.zsh
