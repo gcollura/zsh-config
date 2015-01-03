8b6e1a596eb08d86767276ace09c1f97f7ce00fa
Giulio Collura <random.cpp@gmail.com>
2015-01-03 22:19:45 +0100

    enable vi mode in zsh

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,3 +1,5 @@
+bindkey -v
+
 if [[ "${terminfo[khome]}" != "" ]]; then
     bindkey "${terminfo[khome]}" beginning-of-line # [Home] - Go to beginning of line
 fi
@@ -9,13 +11,6 @@ if [[ "${terminfo[kcbt]}" != "" ]]; then
     bindkey "${terminfo[kcbt]}" reverse-menu-complete # [Shift-Tab] - move through the completion menu backwards
 fi
 
-if [[ "${terminfo[kcuu1]}" != "" ]]; then
-    bindkey "${terminfo[kcuu1]}" up-line-or-search # start typing + [Up-Arrow] - fuzzy find history forward
-fi
-if [[ "${terminfo[kcud1]}" != "" ]]; then
-    bindkey "${terminfo[kcud1]}" down-line-or-search # start typing + [Down-Arrow] - fuzzy find history backward
-fi
-
 bindkey ' ' magic-space # [Space] - do history expansion
 bindkey '^[[1;5C' forward-word # [Ctrl-RightArrow] - move forward one word
 bindkey '^[[1;5D' backward-word # [Ctrl-LeftArrow] - move backward one word
@@ -23,5 +18,5 @@ bindkey '^[[1;5D' backward-word # [Ctrl-LeftArrow] - move backward one word
 # Make vi mode behave sanely
 bindkey "^?" backward-delete-char
 bindkey "^W" backward-kill-word
-# bindkey "^H" backward-delete-char      # Control-h also deletes the previous char
+bindkey "^H" backward-delete-char      # Control-h also deletes the previous char
 bindkey "^U" backward-kill-line
