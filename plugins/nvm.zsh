06e0bceda8a35edff3a83c97f0300ee0cda702d7
Giulio Collura <giulio.collura@gmail.com>
2015-05-25 10:25:08 +0200

    automatically source .nvm/nvm.sh

--- plugins/nvm.zsh
+++ plugins/nvm.zsh
@@ -8,3 +8,6 @@ function nvm_prompt_info() {
     nvm_prompt=${nvm_prompt:1}
     echo "${ZSH_THEME_NVM_PROMPT_PREFIX}${nvm_prompt}${ZSH_THEME_NVM_PROMPT_SUFFIX}"
 }
+
+export NVM_DIR="$HOME/.nvm"
+[[ -s "$NVM_DIR/nvm.sh" ]] && . "$NVM_DIR/nvm.sh"
